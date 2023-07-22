64f689985236e14c9c6f11883a1d6b7e36f1fdae
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-22 17:23:22 +0300

    Done task 2

---
+++ Matrix.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Practice5
+{
+    public class Matrix
+    {
+        private int[,] matrix;
+        private int rows;
+        private int columns;
+
+        public Matrix(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            matrix = new int[rows, columns];
+        }
+
+        public void FillingRandom()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns;j++)
+                {
+                    matrix[i, j] = rand.Next(0, 10);
+                }
+            }
+        }
+
+        public void Output()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns;j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        // Overloaded operator +
+        public static Matrix operator +(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix result = new Matrix(matrix1.rows, matrix1.columns);
+            for (int i = 0; i < matrix1.rows; i++)
+            {
+                for (int j = 0; j < matrix1.columns; j++)
+                {
+                    result.matrix[i, j] = matrix1.matrix[i, j] + matrix2.matrix[i, j];
+                }
+            }
+            return result;
+        }
+        // Overloaded operator - 
+        public static Matrix operator -(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix result = new Matrix(matrix1.rows, matrix1.columns);
+            for (int i = 0; i < matrix1.rows; i++)
+            {
+                for (int j = 0; j < matrix1.columns; j++)
+                {
+                    result.matrix[i, j] = matrix1.matrix[i, j] - matrix2.matrix[i, j];
+                }
+            }
+            return result;
+        }
+
+        // Overloaded operator *
+        public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix result = new Matrix(matrix1.rows, matrix1.columns);
+            for (int i = 0; i < matrix1.rows; i++)
+            {
+                for (int j = 0; j < matrix1.columns; j++)
+                {
+                    result.matrix[i, j] = matrix1.matrix[i, j] * matrix2.matrix[i, j];
+                }
+            }
+            return result;
+        }
+
+        public static Matrix operator *(Matrix matrix , int num)
+        {
+            Matrix result = new Matrix(matrix.rows, matrix.columns);
+            for (int i = 0; i < matrix.rows; i++)
+            {
+                for (int j = 0; j < matrix.columns; j++)
+                {
+                    result.matrix[i, j] = matrix.matrix[i, j] * num;
+                }
+            }
+            return result;
+        }
+
+        // Overloaded operator ==
+        public static bool operator ==(Matrix matrix1, Matrix matrix2)
+        {
+            bool result = false;
+            for (int i = 0; i < matrix1.rows; i++)
+            {
+                for (int j = 0; j < matrix1.columns; j++)
+                {
+                    if (matrix1.matrix[i, j] == matrix2.matrix[i, j])
+                    {
+                        result = true;
+                    }
+                }
+            }
+            return result;
+        }
+
+        // Overloaded operator !=
+        public static bool operator !=(Matrix matrix1, Matrix matrix2)
+        {
+            bool result = false;
+            for (int i = 0; i < matrix1.rows; i++)
+            {
+                for (int j = 0; j < matrix1.columns; j++)
+                {
+                    if (matrix1.matrix[i, j] != matrix2.matrix[i, j])
+                    {
+                        result = true;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Matrix matrix1 = new Matrix(3, 3);
+            Matrix matrix2 = new Matrix(3, 3);
+
+            matrix1.FillingRandom();
+            matrix2.FillingRandom();
+            Console.WriteLine("Matrix 1:");
+            matrix1.Output();
+            Console.WriteLine("Matrix 2:");
+            matrix2.Output();
+
+            // Overloaded operator +
+            Matrix matrix3 = matrix1 + matrix2;
+            Console.WriteLine("Matrix 3 = Matrix 1 + Matrix 2:");
+            matrix3.Output();
+            // Overloaded operator -
+            Matrix matrix4 = matrix1 - matrix2;
+            Console.WriteLine("Matrix 4 = Matrix 1 - Matrix 2:");
+            matrix4.Output();
+            // Overloaded operator *
+            Matrix matrix5 = matrix1 * matrix2;
+            Console.WriteLine("Matrix 5 = Matrix 1 * Matrix 2:");
+            matrix5.Output();
+            // Overloaded operator * (int)
+            Matrix matrix6 = matrix1 * 2;
+            Console.WriteLine("Matrix 6 = Matrix 1 * 2:");
+            matrix6.Output();
+            // Overloaded operator ==
+            Console.WriteLine("Matrix 1 == Matrix 2: " + (matrix1 == matrix2));
+            Console.WriteLine("Matrix 1 == Matrix 1: " + (matrix1 == matrix1));
+            // Overloaded operator !=
+            Console.WriteLine("Matrix 1 != Matrix 2: " + (matrix1 != matrix2));
+            Console.WriteLine("Matrix 1 != Matrix 1: " + (matrix1 != matrix1));
         }
     }
 }
