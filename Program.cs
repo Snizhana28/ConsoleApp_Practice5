9efebcb599aec9ef6b182ee4bc698852d7ed060d
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-22 16:50:08 +0300

    Done task1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Worker worker1 = new Worker("Alex", 22 , 25000);
+            Worker worker2 = new Worker("Bob", 25, 30000);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(worker1);
+            Console.WriteLine(worker2);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Overloaded operators + and -");
+            worker1 = worker1 + 5000;
+            worker2 = worker2 - 5000;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(worker1);
+            Console.WriteLine(worker2);
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Overloaded operators == and !=");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(worker1 == worker2);
+            Console.WriteLine(worker1 != worker2);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Overloaded operators > and <");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(worker1 > worker2);
+            Console.WriteLine(worker1 < worker2);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Overloaded Equals");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(worker1.Equals(worker2));
+            Console.ResetColor();
         }
     }
 }
---
+++ Worker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Practice5
+{
+    public class Worker
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private int Salary { get; set; }
+        public Worker() { }
+        public Worker(string name, int age, int salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+        public static Worker operator +(Worker worker, int salary)
+        {
+            worker.Salary += salary;
+            return worker;
+        }
+        public static Worker operator -(Worker worker, int salary)
+        {
+            worker.Salary -= salary;
+            return worker;
+        }
+        public static bool operator ==(Worker worker1, Worker worker2)
+        {
+            return worker1.Salary == worker2.Salary;
+        }
+        public static bool operator !=(Worker worker1, Worker worker2)
+        {
+            return worker1.Salary != worker2.Salary;
+        }
+        public static bool operator >(Worker worker1, Worker worker2)
+        {
+            return worker1.Salary > worker2.Salary;
+        }
+        public static bool operator <(Worker worker1, Worker worker2)
+        {
+            return worker1.Salary < worker2.Salary;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Worker worker &&
+                   Name == worker.Name &&
+                   Age == worker.Age &&
+                   Salary == worker.Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Salary: {Salary}";
+        }
+    }
+}
