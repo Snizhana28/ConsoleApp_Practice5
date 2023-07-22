e95f6277b47ba48b97e43ae7769347ef540ade17
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-22 16:25:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_Practice5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
