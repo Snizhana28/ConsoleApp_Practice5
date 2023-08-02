ffd13ada7465b4372165f287975e235c02bd033f
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-08-02 19:29:10 +0300

    Done task 3

---
+++ City.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Practice5
+{
+    public class City
+    {
+        private string nameCity;
+        private string nameCountry;
+        private int countPeople;
+        public string NameCity { get; set; }
+        public string NameCountry { get; set; }
+        public int CountPeople { get; set; }
+        public City()
+        {
+            nameCity = "Kyiv";
+            nameCountry = "Ukraine";
+            countPeople = 3000000;
+        }
+        public City(string nameCity, string nameCountry, int countPeople)
+        {
+            this.nameCity = nameCity;
+            this.nameCountry = nameCountry;
+            this.countPeople = countPeople;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter name of city: ");
+            nameCity = Console.ReadLine();
+            Console.WriteLine("Enter name of country: ");
+            nameCountry = Console.ReadLine();
+            Console.WriteLine("Enter count of people: ");
+            countPeople = Convert.ToInt32(Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            return "Name of city: " + nameCity + "\nName of country: " + nameCountry + "\nCount of people: " + countPeople;
+        }
+        public static City operator +(City city, int countPeople)
+        {
+            city.countPeople += countPeople;
+            return city;
+        }
+        public static City operator -(City city, int countPeople)
+        {
+            city.countPeople -= countPeople;
+            return city;
+        }
+        public static bool operator ==(City city1, City city2)
+        {
+            return city1.countPeople == city2.countPeople;
+        }
+        public static bool operator !=(City city1, City city2)
+        {
+            return city1.countPeople != city2.countPeople;
+        }
+        public static bool operator <(City city1, City city2)
+        {
+            return city1.countPeople < city2.countPeople;
+        }
+        public static bool operator >(City city1, City city2)
+        {
+            return city1.countPeople > city2.countPeople;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            City city = new City();
+            city.Input();
+            Console.WriteLine(city.ToString());
+            Console.WriteLine("Enter count of people to add: ");
+            int countPeople = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(city + countPeople);
+            Console.WriteLine("Enter count of people to subtract: ");
+            countPeople = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(city - countPeople);
+            City city1 = new City();
+            city1.Input();
+            Console.WriteLine(city == city1);
+            Console.WriteLine(city != city1);
+            Console.WriteLine(city < city1);
+            Console.WriteLine(city > city1);
         }
     }
 }
