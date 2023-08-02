ad5b2359982e3cd61a401af020c16eda078bb70c
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-08-02 20:05:48 +0300

    Done Task 4

---
+++ CreditCard.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Practice5
+{
+    public class CreditCard
+    {
+        public string Number { get; set; }
+        public string ExpiryDate { get; set; }
+        public string CardHolderName { get; set; }
+        public string CVV { get; set; }
+        public double Money { get; set; }
+        public string BillingAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public CreditCard() { }
+        public CreditCard(string number, string expiryDate, string cardHolderName, string cvv, double money, string billingAddress, string city, string state)
+        {
+            Number = number;
+            ExpiryDate = expiryDate;
+            CardHolderName = cardHolderName;
+            CVV = cvv;
+            Money = money;
+            BillingAddress = billingAddress;
+            City = city;
+            State = state;
+        }
+        public static CreditCard operator +(CreditCard card, double money)
+        {
+            card.Money += money;
+            return card;
+        }
+        public static CreditCard operator -(CreditCard card, double money)
+        {
+            card.Money -= money;
+            return card;
+        }
+        public static bool operator ==(CreditCard card, string cvv)
+        {
+            return card.CVV == cvv;
+        }
+        public static bool operator !=(CreditCard card, string cvv)
+        {
+            return card.CVV != cvv;
+        }
+        public static bool operator >(CreditCard card, double money)
+        {
+            return card.Money > money;
+        }
+        public static bool operator <(CreditCard card, double money)
+        {
+            return card.Money < money;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is CreditCard card &&
+                   Number == card.Number &&
+                   ExpiryDate == card.ExpiryDate &&
+                   CardHolderName == card.CardHolderName &&
+                   CVV == card.CVV &&
+                   BillingAddress == card.BillingAddress &&
+                   City == card.City &&
+                   State == card.State &&
+                   Money == card.Money;
+        }
+        public override string ToString()
+        {
+            return $"Number: {Number}" +
+                $"\nExpiryDate: {ExpiryDate}" +
+                $"\nCardHolderName: {CardHolderName}" +
+                $"\nCVV: {CVV}" +
+                $"\nMoney: {Money}" +
+                $"\nBillingAddress: {BillingAddress}" +
+                $"\nCity: {City}" +
+                $"\nState: {State}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            CreditCard creditCard = new CreditCard();
+            creditCard.Number = "1234 5678 9012 3456";
+            creditCard.ExpiryDate = "12/24";
+            creditCard.CardHolderName = "John Doe";
+            creditCard.CVV = "123";
+            creditCard.Money = 1000;
+            creditCard.BillingAddress = "1234 Main St";
+            creditCard.City = "New York";
+            creditCard.State = "NY";
+            
+            Console.WriteLine("Credit Card Information");
+            Console.WriteLine(creditCard.ToString());
+            // operator +
+            creditCard += 100;
+            Console.WriteLine("Money: " + creditCard.Money);
+            // operator -
+            creditCard -= 100;
+            Console.WriteLine("Money: " + creditCard.Money);
+            // operator ==
+            Console.WriteLine("CVV: " + creditCard.CVV);
+            Console.WriteLine("CVV == 123: " + (creditCard == "123"));
+            // operator !=
+            Console.WriteLine("CVV != 123: " + (creditCard != "123"));
+            // operator >
+            Console.WriteLine("Money > 100: " + (creditCard > 100));
+            // operator <
+            Console.WriteLine("Money < 100: " + (creditCard < 100));
+            // Equals()
+            CreditCard creditCard2 = new CreditCard();
+            creditCard2.Number = "1234 5678 9012 3456";
+            creditCard2.ExpiryDate = "12/24";
+            creditCard2.CardHolderName = "John Doe";
+            creditCard2.CVV = "123";
+            creditCard2.Money = 1000;
+            creditCard2.BillingAddress = "1234 Main St";
+            creditCard2.City = "New York";
+            creditCard2.State = "NY";
+            Console.WriteLine("Equals(): " + creditCard.Equals(creditCard2));
         }
     }
 }
